ef264c58abd277afe5cab345c6c28e4ecbceddda
Apeksha More <moreapeksha75@gmail.com>
2026-02-11 14:41:34 +0530

    Delete functionality add

--- Backend/controllers/transactionController.js
+++ Backend/controllers/transactionController.js
@@ -1,4 +1,4 @@
-import Transaction from "../models/Transaction";
+import Transaction from "../models/Transaction.js";
 
 //Create 
 
@@ -21,11 +21,31 @@ export const createTransaction = async(req, res)=>{
 //Read
 export const getTransaction = async(req, res)=>{
     try{
-        const transactions = await Transaction.find()
+        const transactions = await Transaction.find().sort({ createdAt: -1 });
         res.status(201).json({
           success: true,
           data: transactions,
         });
+    }catch(e){
+        res.status(400).json({
+          success: false,
+          message: e.message,
+        });
+    }
+}
+
+//delete 
+export const deleteTransaction = async(req, res)=>{
+    try{
+        const {id} = req.params
+        
+        const deleteTrans  = await Transaction.findByIdAndDelete(id);
+
+        res.status(201).json({
+            success:true,
+            data:deleteTrans
+        })
+
     }catch(e){
         res.status(400).json({
           success: false,
