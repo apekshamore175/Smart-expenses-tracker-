b010d3b9b60208eba5b5d1e1f07540c8bfd59433
Apeksha More <moreapeksha75@gmail.com>
2026-02-11 08:04:58 +0530

    Model is created

---
+++ Backend/models/Transaction.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const transactionSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["income", "expense"],
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Transaction = mongoose.model("Transaction", transactionSchema);
+
+export default Transaction;
+
