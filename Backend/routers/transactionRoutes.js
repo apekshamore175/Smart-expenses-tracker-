74fbaa7adbaf7fbc517b55fb8a872f751f5a2541
Apeksha More <moreapeksha75@gmail.com>
2026-02-12 08:49:24 +0530

    updated routes

--- Backend/routers/transactionRoutes.js
+++ Backend/routers/transactionRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { createTransaction, getTransaction } from '../controllers/transactionController.js'
+import { createTransaction, deleteTransaction, getTransaction } from '../controllers/transactionController.js'
 
 const route = express.Router()
 
 route.get("/transaction", getTransaction)
 route.post("/transaction", createTransaction)
+route.delete("/transaction/:id", deleteTransaction)
 
 export default route
--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,16 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDb from './config/db.js'
+import transactionRoute from './routers/transactionRoutes.js'
+import cors from 'cors'
 dotenv.config()
 
 const app = express()
 app.use(express.json()) 
+app.use(cors()) 
+
+
+app.use("/api", transactionRoute)
 
 const port = process.env.PORT || 5000 
 
